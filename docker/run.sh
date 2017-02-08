34aa2f8d52453e6f2e3a2efee0cb99a674c8155e
Dan 'Ducky' Little <theduckylittle@gmail.com>
2017-02-08 13:40:27 -0600

    Big clean up. Fix METADATA, clean up quotes

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # run a container that will be deleted on exit.
-docker run -p 127.0.0.1:4000:80 --rm=true --name gm3-demo-data gm3-demo-data 
+docker run -p 127.0.0.1:8000:80 --rm=true -v ~/Projects/GeoMOOSE/gm3-demo-maps:/data --name gm3-demo-data gm3-demo-data 
